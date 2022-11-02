a94f0de2dfda2807915e298c628a95960c97df17
santipo12@gmail.com <santipo12@gmail.com>
2022-11-01 20:16:11 -0500

    mas pipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -8,10 +9,67 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class NoComunesComponent implements OnInit {
 
+  //i18nSelect 
+  nombre:string='Andres';
+  genero:string = 'masculino';
+
+  invitacionMapa = {
+    'masculino':'invitarlo',
+    'femenino':'invitarla'
+  }
+
+  //i18nPlural
+  clientes:string[]=['Maria','Pedro','Santiago','Alejandro']
+  clientesMapa={
+    '=0':'no tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando',
+    'other':'tenemos # clientes esperando'
+  }
+
   constructor(private primengConfig: PrimeNGConfig) { }
 
   ngOnInit(): void {
     this.primengConfig.ripple=true;
   }
 
+  cambiarPersona(){
+    this.nombre='Maria';
+    this.genero='femenino';
+  }
+  borrarCliente(){
+    this.clientes.pop();
+  }
+
+  //Keyvalue Pipe
+
+  persona={
+    nombre:'Santiago',
+    edad:23,
+    direccion:'Pasto, Colombia'
+  }
+
+  //JsonPipe
+  heroes=[
+    {
+      nombre:'Superman',
+      vuela:true,
+    },
+    {
+      nombre:'Robín',
+      vuela:false,
+    },
+    {
+      nombre:'aquaman',
+      vuela:false,
+    }
+  ]
+
+  //Async Pipe
+  miObservable=interval(1000);
+
+  valorPromesa = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('Tenemos data de promesa')
+    }, 3500);
+  });
 }
